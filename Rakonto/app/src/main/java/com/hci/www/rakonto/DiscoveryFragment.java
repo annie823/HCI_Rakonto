5e39cf50f424c425c7a3931a1199b1670d4a52f4
annie823 <annie823123@hotmail.com>
2017-05-17 15:31:09 -0400

    cover flow for discovery

--- Rakonto/app/src/main/java/com/hci/www/rakonto/DiscoveryFragment.java
+++ Rakonto/app/src/main/java/com/hci/www/rakonto/DiscoveryFragment.java
@@ -5,12 +5,73 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.TextSwitcher;
+
+import java.util.ArrayList;
+
+import it.moondroid.coverflow.components.ui.containers.FeatureCoverFlow;
 
 
 public class DiscoveryFragment extends Fragment {
+
+    private FeatureCoverFlow mCoverFlow;
+    private CoverFlowAdapter mAdapter;
+    private ArrayList<String> mData = new ArrayList<>();
+    private TextSwitcher mTitle;
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discovery, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_discovery, container, false);
+
+        mData.add("book 1");
+        mData.add("book 2");
+        mData.add("book 3");
+        mData.add("book 4");
+
+        mAdapter = new CoverFlowAdapter(getActivity().getApplicationContext());
+        mAdapter.setData(mData);
+        FeatureCoverFlow mCoverFlow = (FeatureCoverFlow) rootView.findViewById(R.id.coverflow);
+        mCoverFlow.setAdapter(mAdapter);
+
+        mCoverFlow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //TODO CoverFlow item clicked
+            }
+        });
+
+        mCoverFlow.setOnScrollPositionListener(new FeatureCoverFlow.OnScrollPositionListener() {
+            @Override
+            public void onScrolledToPosition(int position) {
+                //TODO CoverFlow stopped to position
+            }
+
+            @Override
+            public void onScrolling() {
+                //TODO CoverFlow began scrolling
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return rootView;
     }
 }
--- Rakonto/app/src/main/java/com/hci/www/rakonto/MainActivity.java
+++ Rakonto/app/src/main/java/com/hci/www/rakonto/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
 
 import java.util.ArrayList;
 
